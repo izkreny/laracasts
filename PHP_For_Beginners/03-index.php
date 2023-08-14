5e2c0b359cde6ce25f452bdb5b902c6e6466d04a
izkreny <iskren.nemet@gmail.com>
2023-08-14 15:01:32 +0200

    Updated files from orginal source.

--- PHP_For_Beginners/03-index.php
+++ PHP_For_Beginners/03-index.php
@@ -1,16 +1,16 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Demo</title>
+    <title>Your First PHP Tag</title>
 </head>
 <body>
+
     <h1>
         <?php
-            echo "Hello, World!";
+            echo "Hello, World";
         ?>
     </h1>
+
 </body>
 </html>
--- PHP_For_Beginners/04-homework-solution.php
+++ PHP_For_Beginners/04-homework-solution.php
@@ -2,16 +2,17 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Variables</title>
+    <title>Variables - Homework</title>
 </head>
 <body>
 
     <h1>
         <?php
             $greeting = "Hello";
+            $noun = "World";
 
-            echo "$greeting Everybody!"
-        ?>
+            echo "$greeting $noun" . "!"
+        ?> 
     </h1>
 
 </body>
